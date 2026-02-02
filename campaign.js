45fe39a44dd58e77854d68c2ff581043856d27ab
Yaroslav <kya.pk22.6.3@gmail.com>
2026-02-02 12:25:41 +0200

    Update campaign.js

--- campaign.js
+++ campaign.js
@@ -1,152 +1,88 @@
 export const campaign = {
-  data() {
-    return {
-      parent: null,
-      loader: false,
-      ads: [],
-      campaignTitle: ''
-    };
-  },
+    template: `
+        <div>
+            <h2>{{ campaign.title }}</h2>
 
-  mounted() {
-    this.parent = this.$root;
+            <button @click="addRow">New +</button>
 
-    if (!this.parent.user) {
-      this.parent.logout();
-      return;
-    }
+            <table border="1" width="100%" style="margin-top:10px">
+                <tr>
+                    <th>Actions</th>
+                    <th>Fraud</th>
+                    <th>Leads</th>
+                    <th>Clicks</th>
+                    <th>Views</th>
+                    <th>Link</th>
+                    <th>Size</th>
+                    <th>Image</th>
+                    <th>#</th>
+                </tr>
 
-    this.getAds();
-  },
-
-  methods: {
-    getAds() {
-      this.loader = true;
-
-      axios.post(
-        this.parent.url + '/site/getCampaignAds?auth=' + this.parent.user.auth.data,
-        this.parent.toFormData({
-          campaign: this.$route.params.id
-        })
-      ).then(res => {
-        this.ads = Array.isArray(res.data.items) ? res.data.items : [];
-        this.campaignTitle = res.data.title || '';
-        this.loader = false;
-      }).catch(() => {
-        this.parent.logout();
-      });
-    },
-
-    togglePublished(item, value) {
-      item.published = value;
-
-      axios.post(
-        this.parent.url + '/site/actionBanner?auth=' + this.parent.user.auth.data,
-        this.parent.toFormData(item)
-      ).catch(() => {
-        item.published = !value;
-      });
-    },
-
-    remove(item) {
-      if (!confirm('Delete ad?')) return;
-
-      axios.post(
-        this.parent.url + '/site/actionBanner?auth=' + this.parent.user.auth.data,
-        this.parent.toFormData(item)
-      ).then(() => {
-        this.getAds();
-      });
-    }
-  },
-
-  template: `
-  <div class="inside-content">
-    <Header />
-
-    <div class="wrapper">
-      <!-- HEADER -->
-      <div class="flex panel">
-        <div class="w50">
-          <h1>{{ campaignTitle }}</h1>
+                <tr v-for="(row, i) in rows" :key="row.id">
+                    <td>
+                        <button @click="remove(i)">🗑</button>
+                    </td>
+                    <td>{{ row.fraud }}</td>
+                    <td>{{ row.leads }}</td>
+                    <td>{{ row.clicks }}</td>
+                    <td>{{ row.views }}</td>
+                    <td>
+                        <a :href="row.link" target="_blank">{{ row.link }}</a>
+                    </td>
+                    <td>{{ row.size }}</td>
+                    <td>
+                        <img :src="row.image" width="60">
+                    </td>
+                    <td>{{ row.id }}</td>
+                </tr>
+            </table>
         </div>
+    `,
+    data() {
+        return {
+            campaign: {
+                id: null,
+                title: ''
+            },
+            rows: []
+        }
+    },
+    mounted() {
+        const id = this.$route.params.id
 
-        <div class="w50 ar">
-          <a href="#" class="btn" @click.prevent="parent.formData={};">
-            Edit campaign
-          </a>
-          <a href="#" class="btn green" style="margin-left:10px">
-            New +
-          </a>
-        </div>
-      </div>
+        this.campaign = {
+            id,
+            title: 'dreamview-seo'
+        }
 
-      <!-- LOADER -->
-      <div v-if="loader">Loading...</div>
-
-      <!-- ADS TABLE -->
-      <div class="table" v-if="ads.length">
-        <table>
-          <thead>
-            <tr>
-              <th class="actions">Actions</th>
-              <th class="id">Fraud<br>clicks</th>
-              <th class="id">Leads</th>
-              <th class="id">Clicks</th>
-              <th class="id">Views</th>
-              <th>Link</th>
-              <th class="id">Size</th>
-              <th class="image">Image</th>
-              <th class="id">#</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr v-for="ad in ads" :key="ad.id">
-              <td class="actions">
-                ✏️
-                &nbsp;
-                <a href="#" @click.prevent="remove(ad)">🗑</a>
-              </td>
-
-              <td class="id">{{ ad.fclicks || 0 }}</td>
-              <td class="id">{{ ad.leads || 0 }}</td>
-              <td class="id">{{ ad.clicks || 0 }}</td>
-              <td class="id">{{ ad.views || 0 }}</td>
-
-              <td>
-                <a :href="ad.link" target="_blank">
-                  {{ ad.link }}
-                </a>
-              </td>
-
-              <td class="id">
-                {{ ad.width }}x{{ ad.height }}
-              </td>
-
-              <td class="image">
-                <img
-                  :src="ad.image"
-                  style="max-width:60px;max-height:60px"
-                >
-              </td>
-
-              <td class="id">
-                <toogle
-                  :modelValue="ad.published"
-                  @update:modelValue="togglePublished(ad, $event)"
-                />
-                {{ ad.id }}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
-      <div class="empty" v-else>
-        No ads
-      </div>
-    </div>
-  </div>
-  `
-};
+        this.rows = [
+            {
+                id: 6,
+                fraud: 0,
+                leads: 0,
+                clicks: 0,
+                views: 0,
+                link: 'https://dreamview-seo.co.il',
+                size: '320x320',
+                image: 'https://via.placeholder.com/150'
+            }
+        ]
+    },
+    methods: {
+        addRow() {
+            this.rows.push({
+                id: Date.now(),
+                fraud: 0,
+                leads: 0,
+                clicks: 0,
+                views: 0,
+                link: 'https://dreamview-seo.co.il',
+                size: '300x300',
+                image: 'https://via.placeholder.com/150'
+            })
+        },
+        remove(index) {
+            this.rows.splice(index, 1)
+        }
+    }
+}
