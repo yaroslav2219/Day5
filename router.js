4524f20f353d646c478760f4e6d62361b929dea1
Yaroslav <kya.pk22.6.3@gmail.com>
2026-02-02 12:24:27 +0200

    Update router.js

--- router.js
+++ router.js
@@ -1,27 +1,24 @@
-import { login } from './login.js';
-import { campaigns } from './campaigns.js';
-import { campaign } from './campaign.js';
+import { login } from './login.js'
+import { campaigns } from './campaigns.js'
+import { campaign } from './campaign.js'
 
 export const router = VueRouter.createRouter({
     history: VueRouter.createWebHashHistory(),
     routes: [
-        { path: '/', name:'Sign in', component: login },
-        { path: '/campaigns', name:'Campaigns', component: campaigns },
-        { path: '/campaign/:id', name:'Campaign', component: campaign },
-
-          {
+        {
+            path: '/',
+            name: 'Sign in',
+            component: login
+        },
+        {
+            path: '/campaigns',
+            name: 'Campaigns',
+            component: campaigns
+        },
+        {
             path: '/campaign/:id',
-            name: 'Campaign view',
-            component: {
-                template: `<div style="padding:20px">
-                    <h2>Campaign {{ $route.params.id }}</h2>
-                </div>`
-            }
+            name: 'Campaign',
+            component: campaign
         }
     ]
-});
-
-
-
-
-
+})
